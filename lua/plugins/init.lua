3a190bff029e87adaa155c591035a8193c1f5c7c
jesusfl15 <jesus010115@hotmail.es>
2025-08-03 18:33:43 +0200

    Eliminado plugins/init.lua innecesario

--- lua/plugins/init.lua
+++
@@ -1,7 +0,0 @@
--- Archivo: ~/.config/nvim/lua/plugins/init.lua
-
-return {
-  -- Importa todos los archivos .lua de este directorio.
-  -- lazy.nvim buscará un 'return' en cada uno.
-  { import = "plugins" }
-}
