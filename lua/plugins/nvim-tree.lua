13aebb8d8bc52cc104ba0002a0753e8e3c290e95
jesusfl15 <jesus010115@hotmail.es>
2025-08-04 18:48:12 +0200

    Configurar nvim-tree
    Ahora nivm-tree se abre automáticamente al abrir nvim en un directorio

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -4,15 +4,27 @@
 return {
   "nvim-tree/nvim-tree.lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  lazy = false, -- necesitamos esto para poder abrirlo al principio.
   opts = {
+
+    -- Las siguientes líneas no son necesarias por estar activadas por defecto
+    -- pero las dejo comentadas para recordarlas en caso de tener algún
+    -- conflicto futuro.
     -- Deshabilita el explorador de archivos nativo de Neovim
-    disable_netrw = true,
+    -- disable_netrw = true,
+    -- Sustituye netrw por nvim-tree
+    -- hijack_netrw = true,
 
     -- Configuración de la vista
     view = {
       width = 30, -- Ancho del panel lateral
     },
 
+    actions = {
+      open_file ={
+        quit_on_open = true,
+      },
+    },
     -- Configuración visual de los archivos y directorios
     renderer = {
       -- Muestra marcadores de indentación para una vista de árbol más clara
@@ -34,7 +46,7 @@ return {
       dotfiles = false, -- Oculta los archivos que empiezan con un punto
     },
   },
-  
+
   -- Aquí definimos el atajo para abrir y cerrar el panel
   keys = {
     { "<leader>e","<CMD>NvimTreeFindFileToggle<CR>" , desc = "Toggle file explorer" },
