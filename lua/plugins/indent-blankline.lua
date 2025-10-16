3557bab66feb832a897c49fcdb111214161eb4dc
jesusfl15 <jesus010115@hotmail.es>
2025-10-16 16:40:04 +0200

    indent-blankline.lua + lazy-lock

---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,8 @@
+-- Archivo: ~/.config/nvim/lua/plugins/indent-blankline.lua
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+}
