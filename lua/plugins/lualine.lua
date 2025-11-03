982871f746b9c8cfa10acee87620ef4463ac9111
jesusfl15 <jesus010115@hotmail.es>
2025-11-03 15:13:27 +0100

    lualine nvim-tree custom plugin
    intento de plugin custom para que en nvim-tree se vea bien lualine,
    muestra el path y la hora, me gustaría hacer que se ocultase si el focus
    no está en el panel nvim-tree, ya que ya tengo reloj en el buffer
    activo, pero todavía no sé cómo hacer eso.

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,5 +1,25 @@
 -- ~/.config/nvim/lua/plugins/lualine.lua
 
+local function get_short_cwd()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':~')
+end
+local custom_nvimtree = {
+  sections = {
+    lualine_a = { get_short_cwd },
+    lualine_z = {
+      {
+        function()
+          return " " .. os.date("%H:%M")
+        end,
+        refresh = {
+          statusline = 1000,
+        },
+      },
+    }
+  },
+  filetypes = {'NvimTree'}
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "kyazdani42/nvim-web-devicons" },
@@ -8,6 +28,7 @@ return {
       icons_enabled = true,
       theme = "auto",
     },
+    extensions = { custom_nvimtree, 'lazy' },
     sections = {
       lualine_z = { -- mostrar la hora en la barra
         {'location'},
