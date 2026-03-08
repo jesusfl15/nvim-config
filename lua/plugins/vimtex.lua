94c548c098d48a096a7bbd79ac079c39acc25327
jesusfl15 <jesus010115@hotmail.es>
2026-03-08 01:45:22 +0100

    imaps
    cambio imaps_leader de vimtex a ";"
    
    añado un imap de ejemplo desde la documentación, "traducido" a lua

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -5,13 +5,24 @@ return {
   config = function()
     vim.g.vimtex_compiler_latexmk = {
       options = {
-        "-shell-escape",                 -- Permite la ejecución de comandos externos.
-        "-synctex=1",                    -- Habilita la sincronización bidireccional.
-        "-interaction=nonstopmode",      -- Evita que la compilación se detenga por errores.
+        "-shell-escape",            -- Permite la ejecución de comandos externos.
+        "-synctex=1",               -- Habilita la sincronización bidireccional.
+        "-interaction=nonstopmode", -- Evita que la compilación se detenga por errores.
       },
     }
 
+    vim.g.vimtex_imaps_leader = ";"
     vim.g.vimtex_view_method = "zathura"
     vim.g.vimtex_compiler_method = "latexmk"
+
+    -- ejemplo de cómo añadir imaps custom usando lua
+    -- (es un ejemplo de la documentación "traducido" a lua)
+    vim.fn["vimtex#imaps#add_map"]({
+      lhs = "r",
+      rhs = 'vimtex#imaps#style_math("mathrm")',
+      expr = 1,
+      leader = '#',
+      wrapper = 'vimtex#imaps#wrap_math'
+    })
   end,
 }
