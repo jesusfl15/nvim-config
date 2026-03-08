65a55b83e9c20baf91f4953b10b7efa4eb97e73e
jesusfl15 <jesus010115@hotmail.es>
2026-03-08 02:10:31 +0100

    simple_imaps

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -17,12 +17,39 @@ return {
 
     -- ejemplo de cómo añadir imaps custom usando lua
     -- (es un ejemplo de la documentación "traducido" a lua)
-    vim.fn["vimtex#imaps#add_map"]({
-      lhs = "r",
-      rhs = 'vimtex#imaps#style_math("mathrm")',
-      expr = 1,
-      leader = '#',
-      wrapper = 'vimtex#imaps#wrap_math'
-    })
+    -- vim.fn["vimtex#imaps#add_map"]({
+    --   lhs = "r",
+    --   rhs = 'vimtex#imaps#style_math("mathrm")',
+    --   expr = 1,
+    --   leader = '#',
+    --   wrapper = 'vimtex#imaps#wrap_math'
+    -- })
+    --
+    -- registro de un imap (ejemplo)
+    -- 
+    -- vim.fn["vimtex#imaps#add_map"]({
+    --   lhs = "a",
+    --   rhs = "\\alpha",
+    -- })
+
+    -- tabla de imaps simples
+    local simple_imaps = {
+      eureka = "patata"
+    }
+
+    -- creo todos los imaps de simple_imaps
+    -- al no especificar leader, se usa el por defecto
+    -- se puede hacer sin leader haciendo leader = ""
+    local function register_simple_imaps()
+      for lhs, rhs in pairs(simple_imaps) do
+        vim.fn["vimtex#imaps#add_map"]({
+          lhs = lhs,
+          rhs = rhs,
+          -- leader = vim.g.vimtex_imaps_leader,
+        })
+      end
+    end
+
+    register_simple_imaps()
   end,
 }
