6549152ae2cebec4b551503d891f75b3ce06ffa2
jesusfl15 <jesus010115@hotmail.es>
2026-03-16 01:33:45 +0100

    autopairs

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -4,14 +4,21 @@ return {
 
   config = function()
     local npairs = require("nvim-autopairs")
+    -- local rule = require("nvim-autopairs.rule")
 
     npairs.setup({
       check_ts = true,
+      fast_wrap = {},
+      disable_filetype = { "tex" , "markdown" },
     })
 
     -- integración con nvim-cmp
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
     local cmp = require("cmp")
     cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+    -- npairs.add_rules({
+    --   rule("\\[", "\\]", "tex")  -- en latex estorba más de lo que ayuda
+    -- })
   end
 }
