9727ff7a6b8d330067b39260f99bd008e20e4bb3
jesusfl15 <jesus010115@hotmail.es>
2026-03-24 19:14:23 +0100

    auto-save

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -5,8 +5,8 @@ return {
   version = '^1.0.0',                       -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
   cmd = "ASToggle",                         -- optional for lazy loading on command
   opts = {
-    enabled = true,
-    debounce_delay = 500,
+    enabled = false,
+    debounce_delay = 100,
   },
   keys = {
     { "<leader>as", "<cmd>ASToggle<CR>", desc = "Toggle autosave" },
