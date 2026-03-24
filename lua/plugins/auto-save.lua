19bc243be6085f119969f5007e6ff5373df33433
jesusfl15 <jesus010115@hotmail.es>
2026-03-24 19:06:07 +0100

    desactivar carga de auto-save.nvim con filetype

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -3,7 +3,6 @@
 return {
   "okuuva/auto-save.nvim",
   version = '^1.0.0',                       -- see https://devhints.io/semver, alternatively use '*' to use the latest tagged release
-  ft = {"tex"},
   cmd = "ASToggle",                         -- optional for lazy loading on command
   opts = {
     enabled = true,
